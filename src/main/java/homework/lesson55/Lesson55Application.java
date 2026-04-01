a4324cda26bf5ae08f30f1a54d2d413154cf2a7d
dasbkdev <dasmukumjanovvv@gmail.com>
2026-04-01 23:25:51 +0600

    init spring boot project for lesson55

---
+++ src/main/java/homework/lesson55/Lesson55Application.java
@@ -0,0 +1,13 @@
+package homework.lesson55;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson55Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson55Application.class, args);
+    }
+
+}
---
+++ src/test/java/homework/lesson55/Lesson55ApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.lesson55;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson55ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
